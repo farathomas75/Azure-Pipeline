118511a8f0e1d5f1c3676f504ed023fbebb3265b
farathomas75 <fara.thomas75@gmail.com>
2025-10-12 02:49:40 -0400

    Add virtual network and subnet

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   cloud {
-    organization = "FThomas-Pipe"
+    organization = "FThomas-Pipe"   # Remplace par ton organisation Terraform Cloud
     workspaces {
-      name = "FaraSpace"
+      name = "FaraSpace"            # Remplace par ton workspace Terraform Cloud
     }
   }
 }
@@ -18,8 +18,25 @@ provider "azurerm" {
   features {}
 }
 
-# Exemple : création d’un groupe de ressources Azure
+#1. Resource Group (deja present)
 resource "azurerm_resource_group" "rg_demo" {
   name     = "rg-terraform-demo"
-  location = "East US"
+  location = "EAST US"
 }
+
+#2. Virtual Network
+resource "azurerm_virtual_network" "vnet_demo" {
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg_demo.location
+  resource_group_name = azurerm_resource_group.rg_demo.name
+}
+
+#3. Subnet
+resource "azurerm_subnet" "subnet_demo" {
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg_demo.name
+  virtual_network_name = azurerm_virtual_network.vnet_demo.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
