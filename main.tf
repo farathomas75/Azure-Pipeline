dfe630fcaa3fc935911375722dd2514ef763492d
farathomas75 <fara.thomas75@gmail.com>
2025-10-12 02:05:47 -0400

    Add main Terraform configuration

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  cloud {
+    organization = "FThomas-Pipe"
+    workspaces {
+      name = "FaraSpace"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Exemple : création d’un groupe de ressources Azure
+resource "azurerm_resource_group" "rg_demo" {
+  name     = "rg-terraform-demo"
+  location = "East US"
+}
